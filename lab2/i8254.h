3f9c81350e67d21a4f089e96052d166a0b898724
Afonso Neves <up202108884@up.pt>
2025-02-25 13:34:14 +0000

    my i8254 macros

--- lab2/i8254.h
+++ lab2/i8254.h
@@ -1,57 +1,43 @@
 #ifndef _LCOM_I8254_H_
 #define _LCOM_I8254_H_
 
-#include <lcom/lcf.h>
+/* General Macros */
 
-/** @defgroup i8254 i8254
- * @{
- *
- * Constants for programming the i8254 Timer. Needs to be completed.
- */
+#define TIMER_IRQ  0       /**< @brief Timer 0 IRQ line */
+#define TIMER_FREQ 1193182 /**< @brief Default Timer 0 frequency */
 
-#define TIMER_FREQ 1193182 /**< @brief clock frequency for timer in PC and AT */
-#define TIMER0_IRQ 0 /**< @brief Timer 0 IRQ line */
+/* Port Addresses */
 
-/* I/O port addresses */
-
-#define TIMER_0    0x40 /**< @brief Timer 0 count register */
-#define TIMER_1    0x41 /**< @brief Timer 1 count register */
-#define TIMER_2    0x42 /**< @brief Timer 2 count register */
+#define TIMER_0    0x40 /**< @brief Timer 0 register */
 #define TIMER_CTRL 0x43 /**< @brief Control register */
 
-#define SPEAKER_CTRL 0x61 /**< @brief Register for speaker control  */
+/* Timer Control */
 
-/* Timer control */
+/* Timer Selection: Bits 7 and 6 */
 
-/* Timer selection: bits 7 and 6 */
+#define TIMER_SEL0 0x00 /**< @brief Control Word for Timer 0 */
 
-#define TIMER_SEL0   0x00              /**< @brief Control Word for Timer 0 */
-#define TIMER_SEL1   BIT(6)            /**< @brief Control Word for Timer 1 */
-#define TIMER_SEL2   BIT(7)            /**< @brief Control Word for Timer 2 */
-#define TIMER_RB_CMD (BIT(7) | BIT(6)) /**< @brief Read Back Command */
-
-/* Register selection: bits 5 and 4 */
+/* Register Selection: Bits 5 and 4 */
 
 #define TIMER_LSB     BIT(4)                  /**< @brief Initialize Counter LSB only */
 #define TIMER_MSB     BIT(5)                  /**< @brief Initialize Counter MSB only */
 #define TIMER_LSB_MSB (TIMER_LSB | TIMER_MSB) /**< @brief Initialize LSB first and MSB afterwards */
 
-/* Operating mode: bits 3, 2 and 1 */
+/* Operating Mode Selection: Bits 3, 2 and 1 */
 
-#define TIMER_SQR_WAVE (BIT(2) | BIT(1)) /**< @brief Mode 3: square wave generator */
-#define TIMER_RATE_GEN BIT(2)            /**< @brief Mode 2: rate generator */
+#define TIMER_SQR_WAVE (BIT(2) | BIT(1)) /**< @brief Mode 3: Square Wave Generator Mode */
+#define TIMER_RATE_GEN BIT(2)            /**< @brief Mode 2: Rate Generator Mode */
 
-/* Counting mode: bit 0 */
+/* Counting Mode Selection: Bit 0 */
 
-#define TIMER_BCD 0x01 /**< @brief Count in BCD */
-#define TIMER_BIN 0x00 /**< @brief Count in binary */
+#define TIMER_BCD 0x01 /**< @brief BCD Mode */
+#define TIMER_BIN 0x00 /**< @brief Binary Mode */
 
-/* READ-BACK COMMAND FORMAT */
+/* Read-Back Command */
 
-#define TIMER_RB_COUNT_  BIT(5)
-#define TIMER_RB_STATUS_ BIT(4)
-#define TIMER_RB_SEL(n)  BIT((n) + 1)
+#define TIMER_RB_SEL0   BIT(1)            /**< @brief Select Timer 0 */
+#define TIMER_RB_COUNT  BIT(4)            /**< @brief Read Count Value */
+#define TIMER_RB_STATUS BIT(5)            /**< @brief Read Status Value */
+#define TIMER_RB_CMD    (BIT(7) | BIT(6)) /**< @brief Read Back Command */
 
-/**@}*/
-
-#endif /* _LCOM_I8254_H */
+#endif /* _LCOM_I8254_H_ */
