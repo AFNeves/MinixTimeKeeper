363d1e9b98ae368c4016ca5e5bb5eb1e98d9bfe9
Tiago Oliveira <up202009302@up.pt>
2025-03-08 22:48:46 +0000

    utils.c done

---
+++ lab2/utils.c
@@ -0,0 +1,26 @@
+#include <lcom/lcf.h>
+
+#include <stdint.h>
+
+int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
+  if (lsb == NULL) return 1;
+  *lsb = val & 0x0F;
+  return 0;
+}
+
+int(util_get_MSB)(uint16_t val, uint8_t *msb) {
+  if(msb == NULL) return 1;
+  uint8_t tmp = val >> 8;
+  *msb = tmp;
+  return 0;
+} 
+
+int (util_sys_inb)(int port, uint8_t *value) {
+  if (value == NULL) return 1;
+
+  uint32_t tmp;
+  int r = sys_inb(port, &tmp);
+  *value = (uint8_t) tmp;
+
+  return r;
+}
