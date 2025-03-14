a8f8aa2ba8b897fb6d0e3a919683022e046145e2
Tiago Oliveira <up202009302@up.pt>
2025-03-14 11:27:26 +0000

    Fixed a mistake

--- lab2/timer.c
+++ lab2/timer.c
@@ -5,108 +5,120 @@
 
 #include "i8254.h"
 
-int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
-  if (timer < 0 || timer > 2 || freq < 19 || freq > TIMER_FREQ) return 1;
+int hook_id = 0;
+int counter = 0;
 
-  uint16_t newFreq = TIMER_FREQ / freq;
-  uint8_t control_word, lsb, msb;
+int (timer_set_frequency)(uint8_t timer, uint32_t freq)
+{
+    if (freq > TIMER_FREQ) return 1;
 
-  util_get_LSB(newFreq, &lsb);
-  util_get_MSB(newFreq, &msb);
+    uint8_t controlWord;
+    if (timer_get_conf(timer, &controlWord) != 0) return 1;
+    controlWord = (controlWord & 0x0F) | TIMER_LSB_MSB;
 
-  if(timer_get_conf(timer, &control_word) != 0) return 1;
-  control_word = control_word & 0xF;
-  control_word = TIMER_LSB_MSB;
+    uint16_t val = TIMER_FREQ / freq;
+    uint8_t LSB, MSB;
+    util_get_LSB(val, &LSB);
+    util_get_MSB(val, &MSB);
 
-  switch(timer){
-    case 0:
-      control_word |= TIMER_SEL0;
-      break;
-    case 1:
-      control_word |= TIMER_SEL1;
-      break;
-    case 2:
-      control_word |= TIMER_SEL2;
-      break;
-    default:
-      break;
-  }
+    uint8_t selectedTimer;
+    switch (timer)
+    {
+        case 0:
+            controlWord |= TIMER_SEL0;
+            selectedTimer = TIMER_0;
+            break;
 
-  if(sys_outb(TIMER_CTRL, control_word) != 0) return 1;
-  if(sys_outb(TIMER_0 + timer, lsb) != 0) return 1;
-  if(sys_outb(TIMER_0 + timer, msb) != 0) return 1;
+        case 1:
+            controlWord |= TIMER_SEL1;
+            selectedTimer = TIMER_1;
+            break;
 
-    
-  return 0;
+        case 2:
+            controlWord |= TIMER_SEL2;
+            selectedTimer = TIMER_2;
+            break;
+
+        default:
+            return 1;
+    }
+
+    if (sys_outb(TIMER_CTRL, controlWord) != 0) return 1;
+    if (sys_outb(selectedTimer, LSB) != 0) return 1;
+    if (sys_outb(selectedTimer, MSB) != 0) return 1;
+
+    return 0;
 }
 
-int (timer_subscribe_int)(uint8_t *bit_no) {
-    /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+int (timer_subscribe_int)(uint8_t *bit_no)
+{
+    if (bit_no == NULL) return 1;
 
-  return 1;
+    *bit_no = hook_id;
+
+    return sys_irqsetpolicy(TIMER_IRQ, IRQ_REENABLE, &hook_id);
 }
 
-int (timer_unsubscribe_int)() {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+int (timer_unsubscribe_int)()
+{
+    return sys_irqrmpolicy(&hook_id);
 }
 
-void (timer_int_handler)() {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+void (timer_int_handler)()
+{
+    counter++;
 }
 
-int (timer_get_conf)(uint8_t timer, uint8_t *config) {
-  if(timer < 0 || timer > 2 || config == NULL) return 1;
+int (timer_get_conf)(uint8_t timer, uint8_t *st)
+{
+    if (st == NULL || timer > 2) return 1;
 
-  uint8_t rbc = TIMER_RB_CMD | TIMER_RB_COUNT_ | TIMER_RB_SEL(timer); 
-  if(sys_outb(TIMER_CTRL, rbc) != 0) return 1;
-  if(util_sys_inb(TIMER_0 + timer, config) != 0) return 1;
+    uint8_t readBack = TIMER_RB_CMD | TIMER_RB_COUNT_ | TIMER_RB_SEL(timer);
 
-  return 0;
+    if (sys_outb(TIMER_CTRL, readBack) != 0) return 1;
+
+    if (util_sys_inb(TIMER_0 + timer, st) != 0) return 1;
+
+    return 0;
 }
 
-int (timer_display_conf)(uint8_t timer, uint8_t config,
-                        enum timer_status_field field) {
-  
-  union timer_status_field_val data;
+int (timer_display_conf)(uint8_t timer, uint8_t st, enum timer_status_field field)
+{
+    union timer_status_field_val data;
+    switch (field)
+    {
+        case tsf_all:
+            data.byte = st;
+            break;
+        
+        case tsf_initial:
+            st = (st >> 4) & 0x03; // Get bit 5 and 4
 
-  switch(field){
-    case tsf_all:
-      data.byte = config;
-      break;
+            if (st == 1) data.in_mode = LSB_only;
+            else if (st == 2) data.in_mode = MSB_only;
+            else if (st == 3) data.in_mode = MSB_after_LSB;
+            else data.in_mode = INVAL_val;
 
-    case tsf_initial:
-      config = (config >> 4) & 0x03;
+            break;
+        
+        case tsf_mode:
+            st = (st >> 1) & 0x07; // Get bit 3, 2 and 1
 
-      if(config == 1) data.in_mode = LSB_only;
-      else if(config == 2) data.in_mode = MSB_only;
-      else if(config == 3) data.in_mode = MSB_after_LSB;
-      else data.in_mode = INVAL_val;
+            if (st == 6) data.count_mode = 2;
+            else if (st == 7) data.count_mode = 3;
+            else data.count_mode = st;
 
-      break;
+            break;
 
-    case tsf_mode:
-      config = (config >> 1) & 0x07;
+        case tsf_base:
+            data.bcd = st & TIMER_BCD;
+            break;
+        
+        default:
+            return 1;
+    }
 
-      if(config == 6) data.count_mode = 2;
-      else if(config == 7) data.count_mode = 3;
-      else data.count_mode = config;
-      
-      break;
+    if (timer_print_config(timer, field, data) != 0) return 1;
 
-    case tsf_base:
-      data.bcd = config & TIMER_BCD;
-      break;
-
-    default:
-      return 1;
-  }
-
-  if(timer_print_config(timer, field, data) != 0) return 1;
-
-  return 0;
+    return 0;
 }
