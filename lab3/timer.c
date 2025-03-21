df05deb8e986fbf28d8d17ee3c98f1d8fa131ff2
Afonso Neves <up202108884@up.pt>
2025-03-21 13:00:31 +0000

    timer_set_freq revision

--- lab3/timer.c
+++ lab3/timer.c
@@ -14,38 +14,16 @@ int (timer_set_frequency)(uint8_t timer, uint32_t freq)
 
     uint8_t controlWord;
     if (timer_get_conf(timer, &controlWord) != 0) return 1;
-    controlWord = (controlWord & 0x0F) | TIMER_LSB_MSB;
+    controlWord = (controlWord & 0xCF) | TIMER_LSB_MSB;
 
     uint16_t val = TIMER_FREQ / freq;
     uint8_t LSB, MSB;
     util_get_LSB(val, &LSB);
     util_get_MSB(val, &MSB);
 
-    uint8_t selectedTimer;
-    switch (timer)
-    {
-        case 0:
-            controlWord |= TIMER_SEL0;
-            selectedTimer = TIMER_0;
-            break;
-
-        case 1:
-            controlWord |= TIMER_SEL1;
-            selectedTimer = TIMER_1;
-            break;
-
-        case 2:
-            controlWord |= TIMER_SEL2;
-            selectedTimer = TIMER_2;
-            break;
-
-        default:
-            return 1;
-    }
-
     if (sys_outb(TIMER_CTRL, controlWord) != 0) return 1;
-    if (sys_outb(selectedTimer, LSB) != 0) return 1;
-    if (sys_outb(selectedTimer, MSB) != 0) return 1;
+    if (sys_outb(TIMER_0 + timer, LSB) != 0) return 1;
+    if (sys_outb(TIMER_0 + timer, MSB) != 0) return 1;
 
     return 0;
 }
