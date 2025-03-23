f2623354ec12a6116231c3b65ea71d4f4322ad5c
Afonso Neves <up202108884@up.pt>
2025-03-23 17:58:57 +0000

    lab4 initial tweaks

--- lab4/KBC.c
+++ lab4/KBC.c
@@ -1,26 +1,4 @@
-#include "keyboard.h"
-
-uint8_t scancode;
-int kb_hook_id = 1;
-
-int (keyboard_subscribe_int)(uint8_t *bit_no)
-{
-    if (bit_no == NULL) return 1;
-
-    *bit_no = kb_hook_id;
-
-    return sys_irqsetpolicy(KB_IRQ, IRQ_REENABLE | IRQ_EXCLUSIVE, &kb_hook_id);
-}
-
-int (keyboard_unsubscribe_int)()
-{
-    return sys_irqrmpolicy(&kb_hook_id);
-}
-
-void (kbc_ih)()
-{
-    read_KBC_output(&scancode);
-}
+#include "KBC.h"
 
 int (read_KBC_output)(uint8_t *byte)
 {
@@ -76,26 +54,4 @@ int (write_to_KBC)(uint8_t port, uint8_t command)
     }
 
     return 1;
-}
-
-int (keyboard_poll_restore)()
-{
-    uint8_t command;
-
-    // Ask permission to read the command byte
-    if (write_to_KBC(KBC_IN_BUF, KBC_READ_CMD) != 0) return 1;
-
-    // Read the command byte
-    if (read_KBC_output(&command) != 0) return 1;
-
-    // Enable interrupts via bit masking
-    command |= KBC_KB_INT;
-
-    // Ask permission to write the command byte
-    if (write_to_KBC(KBC_IN_BUF, KBC_WRITE_CMD) != 0) return 1;
-
-    // Write the command byte
-    if (write_to_KBC(KBC_IN_BUF_ARG, command) != 0) return 1;
-
-    return 0;
 }
---
+++ lab4/KBC.h
@@ -0,0 +1,14 @@
+#ifndef _LCOM_KBC_H_
+#define _LCOM_KBC_H_
+
+#include <lcom/lcf.h>
+
+#include <stdint.h>
+
+#include "i8042.h"
+
+int (read_KBC_output)(uint8_t *byte);
+
+int (write_to_KBC)(uint8_t port, uint8_t command);
+
+#endif /* _LCOM_KBC_H_ */
